48728f35ebf9d8f633af6eff5c8075bec201e8f1
Eric Iacutone <eric.iacutone@gmail.com>
2013-06-20 15:35:58 -0400

    impress.js thank you note working

---
+++ app/controllers/thanks_controller.rb
@@ -0,0 +1,5 @@
+class ThanksController < ApplicationController
+
+	def new
+	end
+end
--- config/initializers/thanks.js
+++ config/initializers/thanks.js
@@ -1,19 +1,13 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree
-
+/**
+ * impress.js
+ *
+ * impress.js is a presentation tool based on the power of CSS3 transforms and transitions
+ * in modern browsers and inspired by the idea behind prezi.com.
+ *
+ * MIT Licensed.
+ *
+ * Copyright 2011 Bartek Szopka (@bartaz)
+ */
 
 (function ( document, window ) {
 
@@ -327,3 +321,4 @@
     select(getElementFromUrl() || steps[0]);
 
 })(document, window);
+
